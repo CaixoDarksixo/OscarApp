4f01fd098bda5a9a2b63b5a8a1b5cadc041b956b
CaixoDarksixo <jpnossol@gmail.com>
2026-06-14 15:24:25 -0300

    feat: criação do sistema central

---
+++ backend/SistemaCentral/src/test/kotlin/com/mobile/sistemacentral/SistemaCentralApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mobile.sistemacentral
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemaCentralApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
